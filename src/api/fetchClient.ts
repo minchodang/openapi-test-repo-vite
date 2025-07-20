bd9bde6e3472c5c10e929bf64dff8d7a59f8e286
minsuKang <90169703+minchodang@users.noreply.github.com>
2025-07-20 10:32:34 +0900

    feat: :sparkles: infiniteQueryOptions with test code add (#2)

---
+++ src/api/fetchClient.ts
@@ -0,0 +1,48 @@
+import type { paths } from './../../test/fixtures/api.d';
+import createFetchClient from 'openapi-fetch';
+import createClient from './index';
+
+const fetchClient = createFetchClient<paths>({
+  baseUrl: 'https://api.example.com',
+});
+
+const $api = createClient(fetchClient);
+
+const infiniteOptions = $api.infiniteQueryOptions(
+  'get',
+  '/paginated-data',
+  {
+    params: {
+      query: {
+        limit: 3,
+      },
+    },
+  },
+  {
+    getNextPageParam: () => {
+      return '3';
+    },
+    initialPageParam: '2',
+  }
+);
+
+const test2 = $api.useInfiniteQuery(
+  'get',
+  '/paginated-data',
+  {
+    params: {
+      query: {
+        limit: 3,
+      },
+    },
+  },
+  {
+    getNextPageParam: () => {
+      return 2;
+    },
+    getPreviousPageParam: () => {
+      return 3;
+    },
+    initialPageParam: 3,
+  }
+);
